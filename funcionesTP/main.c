c902880d2b8160c4100fd7236fc5b2e497508570
Matias Guliano <matiastrex1@gmail.com>
2024-10-24 14:53:16 -0300

    Probando funciones de musica
    Se utilizan las librerias window.h y <MMsystem.h>
    Además, se debe agregar en build options del proyecto > linker settings > other linker options, el comando "-winmm"

--- funcionesTP/main.c
+++ funcionesTP/main.c
@@ -1,29 +1,33 @@
-#include "listaDinamica.h"
 #include "funcionesJuego.h"
-
+#include "musica.h"
 
 int main()
 {
-    FILE* archivo;
-    archivo = generarArchivoDeInforme("informe-juego");
+    char comando[256];
+    const char* archivo = "Music/musica.mp3";
 
+    // TRUE -> Reproduce archivo WAV usando PlaySound
+    if(strstr(archivo,".wav") != NULL)
+    {
+        PlaySound(archivo,NULL,SND_FILENAME | SND_ASYNC);
+        printf("Reproduciendo WAV:\n");
+
+        printf("Presiona Enter para detener la musica...\n");
+        getchar();
+    }
+    // FALSE -> Comando para reproducir MP3 con MCI (Media Control Interface)
+    else
+    {
+        sprintf(comando, "open \"%s\" type mpegvideo alias mp3", archivo);
+        mciSendString(comando, NULL, 0, NULL);
+
+        mciSendString("play mp3", NULL, 0, NULL);
+        printf("Reproduciendo MP3:\n");
+
+        printf("Presiona Enter para detener la musica...\n");
+        getchar();
+    }
 
     return 0;
 }
 
-//    int numeros[10];
-//    int ce = 5;
-//    tLista listaSec1, listaSec2;
-//    tCola cola;
-//    char secuencia1[15];
-//    char secuencia2[15];
-//
-//    crearCola(&cola);
-//    crearLista(&listaSec1);
-//    crearLista(&listaSec2);
-//
-//    obtenerNumerosAleatorios(numeros, ce, 1, 10);
-//    puts("");
-//    for(int i=0; i<ce; i++)
-//        printf("%d ", numeros[i]);
-//    puts("");
---
+++ funcionesTP/musica.h
@@ -0,0 +1,8 @@
+#ifndef MUSICA_H_INCLUDED
+#define MUSICA_H_INCLUDED
+
+#include <stdio.h>
+#include <windows.h>
+#include <MMsystem.h>
+
+#endif // MUSICA_H_INCLUDED
