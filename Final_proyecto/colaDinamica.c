2b07a2e7162485cfc2ab49b972decc7b174598b8
modestti <tomas.modestti@gmail.com>
2024-10-25 20:48:43 -0300

    Mostrar ganadores y exportar a archivos final

---
+++ Final_proyecto/colaDinamica.c
@@ -0,0 +1,147 @@
+#include "colaDinamica.h"
+
+#define MIN(x,y) (((x)<(y))? (x) : (y))
+
+void crearCola(tCola* cola)
+{
+    cola->pri = NULL;
+    cola->ult = NULL;
+}
+int colaVacia(const tCola* cola)
+{
+    return cola->pri == NULL;
+}
+int colaLlena(const tCola* cola, unsigned tam)
+{
+    tNodoCola* nodoAux = (tNodoCola*)malloc(sizeof(tNodoCola));
+    if(!nodoAux)
+    {
+        return 1;
+    }
+    nodoAux->info = malloc(tam);
+    if(!nodoAux->info)
+    {
+        free(nodoAux);
+        return 1;
+    }
+    free(nodoAux->info);
+    free(nodoAux);
+    return 0;
+}
+//versión corta
+//int colaLlena(const tCola* cola, unsigned tam)
+//{
+//    return HAY_MEMORIA;
+//}
+
+int ponerEnCola(tCola* cola, unsigned tam, const void* info)
+{
+    tNodoCola* nue = (tNodoCola*)malloc(sizeof(tNodoCola)); //Nodo nuevo
+    if(!nue)
+    {
+        return 0;
+    }
+    nue->info = malloc(tam);
+    if(!nue->info)
+    {
+        free(nue);
+        return 0;
+    }
+
+    memcpy(nue->info,info,tam);
+    nue->tamInfo = tam;
+    nue->sig = NULL;
+
+    if(cola->pri == NULL)
+    {
+        cola->pri = nue;
+    }
+    else
+    {
+        cola->ult->sig = nue;
+    }
+    cola->ult = nue;
+
+    return 1;
+}
+int sacarDeCola(tCola* cola, unsigned tamBuffer, void* buffer)
+{
+    tNodoCola* auxElim;
+    if(cola->pri==NULL)
+    {
+        return 0;
+    }
+
+    auxElim = cola->pri;
+    memcpy(buffer,auxElim->info,MIN(tamBuffer,auxElim->tamInfo));
+    cola->pri = auxElim->sig;
+
+    free(auxElim->info);
+    free(auxElim);
+
+    if(cola->pri==NULL)
+    {
+        cola->ult = NULL;
+    }
+    return 1;
+}
+int verPrimero(const tCola* cola, unsigned tamBuffer, void* buffer)
+{
+    if(cola->pri == NULL)
+    {
+        buffer = NULL;
+        return 0;
+    }
+    memcpy(buffer,cola->pri,MIN(tamBuffer,cola->pri->tamInfo));
+    return 1;
+}
+void vaciarCola(tCola* cola)
+{
+    tNodoCola* auxElim;
+    while(cola->pri)
+    {
+        auxElim = cola->pri;
+        cola->pri = auxElim->sig;
+        free(auxElim->info);
+        free(auxElim);
+    }
+    cola->ult = NULL;
+}
+int cargarArchivoBinarioEnCola(tCola* cola, const char* path, unsigned tam)
+{
+    void* buffer;
+    FILE* pf = fopen(path,"rb");
+    if(pf == NULL)
+    {
+        return 0;
+    }
+    buffer = malloc(tam);
+    if(buffer == NULL)
+    {
+        fclose(pf);
+        return 0;
+    }
+    fread(buffer,tam,1,pf);
+    while(!feof(pf))
+    {
+        ponerEnCola(cola,tam,buffer);
+        fread(buffer,tam,1,pf);
+    }
+    fclose(pf);
+    return 1;
+}
+int recorrerYMostrarColaUniforme(tCola* cola,void accion(const void* a),unsigned tam)
+{
+    void* buffer = malloc(tam);
+    if(buffer == NULL)
+    {
+        return 0;
+    }
+    while(!colaVacia(cola))
+    {
+        sacarDeCola(cola,tam,buffer);
+        accion(buffer);
+    }
+    return 1;
+}
+
---
+++ Final_proyecto/listaDinamica.h
@@ -0,0 +1,45 @@
+#ifndef LISTADINAMICA_H_INCLUDED
+#define LISTADINAMICA_H_INCLUDED
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define TODO_OK 0
+#define SIN_MEM 1
+#define SIN_INFO 1
+#define MIN(x,y) ((x)<=(y)? (x) : (y))
+#define ASC 1
+#define DESC -1
+
+typedef struct s_nodo
+{
+    unsigned tam;
+    void* info;
+    struct s_nodo* sig;
+}tNodo;
+
+typedef tNodo* tLista;
+
+void crearLista(tLista* lista);
+void vaciarLista(tLista* lista);
+int esListaVacia(const tLista* lista);
+int compararListas(const tLista* lista1, const tLista* lista2, int cmp (const void* a, const void* b));
+void cargarListaConEnteros(tLista* lista);
+void cargarListaConSecuencia(tLista* lista, const char* secuencia);
+
+int agregarAlInicioDeLista(tLista* lista, void* dato, unsigned tam);
+int agregarAlFinalDeLista(tLista* lista, void* dato, unsigned tam);
+int insertarEnListaOrdenada(tLista* lista, const void* dato, unsigned tamDato, int duplicado, int cmp(const void* a,const void* b));
+
+void recorrerLista(tLista* lista, void accion(const void*a,const void*b));
+
+int eliminarDeListaOrdenadaPorClave(tLista* lista, void* buffer, unsigned tamDato, int cmp(const void* a,const void* b));
+int eliminarPrimeroDeLista(tLista* l, void* info, unsigned tam_info);
+
+void ordenarListaPorSeleccion(tLista* lista, int cmp(const void* a,const void* b),int sentido);
+tNodo* buscarMenor(const tNodo* nodoInicial, int cmp(const void*, const void*),int sentido);
+void intercambiarInfoDeNodos(void** a, void** b);
+
+void accion(void*a, void*b);
+
+#endif // LISTADINAMICA_H_INCLUDED
