a2b626e0c4e7c8c66bf045ceab4fe8ebe5da81c2
Matias Guliano <matiastrex1@gmail.com>
2024-10-27 21:18:36 -0300

    Incorporacion musica

--- CmonFinal/main.c
+++ CmonFinal/main.c
@@ -35,6 +35,9 @@ int main()
     /// Generar archivo informe
     datosPartida.archInforme = generarArchivoDeInforme("informe.txt");
 
+    /// Reproducir Musica (VERIFICAR QUE -lwinmm ESTÉ AGREGADO COMO PARÁMETRO EN BUILD OPTIONS > LINKER SETTINGS > OTHER LINK OPTIONS
+    reproducirMusica(MUSICA1);
+
     /// Jugar partidas
     datosPartida.maximaPuntuacion = 0;
     mapLista(&listaJugadores, &datosPartida, jugarPartida);
---
+++ CmonFinal/musica/musica.h
@@ -0,0 +1,14 @@
+#ifndef MUSICA_H_INCLUDED
+#define MUSICA_H_INCLUDED
+
+#include <stdio.h>
+#include <windows.h>
+#include <MMsystem.h>
+
+#define MUSICA1 "musica/halloween.mp3"
+#define MUSICA2 "musica/sinisterPuzzle.mp3"
+#define MUSICA3 "musica/spookyToyland.mp3"
+#define MUSICA4 "musica/underCorruption.mp3"
+#define MUSICA5 "musica/corruption.mp3"
+void reproducirMusica(const char* musica);
+#endif // MUSICA_H_INCLUDED
